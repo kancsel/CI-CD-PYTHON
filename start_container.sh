d68e9c7bdb04734aab4309e8246d8a51f954c9c2
kancsel <kancsel@gmail.com>
2024-03-21 22:48:05 +0530

    Update start_container.sh
    added start_container script

--- start_container.sh
+++ start_container.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -e
 
+# Pull the Docker image from Docker Hub
+echo "pulling"
+docker pull kprincess1/simple-python-flask-app
+
+
+# Run the Docker image as a container
+echo "running"
+docker run -d -p kprincess1/simple-python-flask-app
