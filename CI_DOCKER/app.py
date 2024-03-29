1bb79ccd9b164f3d6b82aaf7fd85365f5e9ac7c3
kancsel <kancsel@gmail.com>
2024-03-29 17:31:12 +0530

    Update app.py

--- CI_DOCKER/app.py
+++ CI_DOCKER/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!,MONOTSUKURI ganbatte'
+    return 'Hello, world!,MONOTSUKURI ganbatte  '
 
 if __name__ == '__main__':
     app.run()
